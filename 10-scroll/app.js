41d8e99a837ed218cd792f263eeb16353ebd8dde
Oliver <oliverahmed000@gmail.com>
2022-02-24 13:50:14 +0600

    made navbar responsive, X button added on menu button

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -4,4 +4,9 @@ const navToggle = document.getElementById("nav-toggle");
 
 navToggle.addEventListener("click", function () {
   linksContainer.classList.toggle("show-links");
+  if (linksContainer.classList.contains("show-links")) {
+    navToggle.innerHTML = `<i class="fas fa-times"></i>`;
+  } else {
+    navToggle.innerHTML = `<i class="fas fa-solid fa-bars"></i>`;
+  }
 });
